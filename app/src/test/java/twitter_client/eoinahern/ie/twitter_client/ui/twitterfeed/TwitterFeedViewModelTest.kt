f89d415191efc61077ead6f3c55511fa4645a529
eoinahern <eoin.ahern@mycit.ie>
2018-11-30 15:45:21 +0000

    added viewmodel test

--- app/src/test/java/twitter_client/eoinahern/ie/twitter_client/ui/twitterfeed/TwitterFeedViewModelTest.kt
+++ app/src/test/java/twitter_client/eoinahern/ie/twitter_client/ui/twitterfeed/TwitterFeedViewModelTest.kt
@@ -5,15 +5,52 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import twitter_client.eoinahern.ie.twitter_client.data.database.TweetDao
+import twitter_client.eoinahern.ie.twitter_client.data.sharedprefs.SharedPrefsHelper
+import twitter_client.eoinahern.ie.twitter_client.domain.DeleteAllTweetsInteractor
+import twitter_client.eoinahern.ie.twitter_client.domain.DeleteExpiredTweetsInteractor
+import twitter_client.eoinahern.ie.twitter_client.domain.GetTwitterDataInteractor
+import twitter_client.eoinahern.ie.twitter_client.tools.resources.ResourceProvider
 
 class TwitterFeedViewModelTest {
 
+    @Mock
+    lateinit var mockDeleteAllTweetsInteractor: DeleteAllTweetsInteractor
+
+    @Mock
+    lateinit var mockDeleteExpiredTweetsInteractor: DeleteExpiredTweetsInteractor
+
+    @Mock
+    lateinit var mockGetTwitterDataInteractor: GetTwitterDataInteractor
+
+    @Mock
+    lateinit var mockTweetDao: TweetDao
+
+    @Mock
+    lateinit var mockSharedPrefsHelper: SharedPrefsHelper
+
+    @Mock
+    lateinit var mockResourceProvider: ResourceProvider
+
+    lateinit var twitterFeedViewModel: TwitterFeedViewModel
+
     @Before
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        twitterFeedViewModel = TwitterFeedViewModel(
+            mockGetTwitterDataInteractor, mockDeleteAllTweetsInteractor,
+            mockDeleteExpiredTweetsInteractor, mockTweetDao, mockSharedPrefsHelper, mockResourceProvider
+        )
     }
 
     @Test
     fun setTTLTime() {
+        twitterFeedViewModel.setTTLTime(1L)
+        Mockito.verify(mockSharedPrefsHelper).saveLong(Mockito.anyString(), Mockito.anyLong())
     }
 
     @Test
